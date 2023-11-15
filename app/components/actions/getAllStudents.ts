9e356ebd9ed651d8331185845a46e172feb61689
chim347 <nhanntse160792@gmail.com>
2023-11-16 05:41:15 +0700

    dashboard manager student

---
+++ app/components/actions/getAllStudents.ts
@@ -0,0 +1,16 @@
+export default async function getAllStudents() {
+  const response = await fetch(
+    "http://13.210.56.232/api/v1/students/get-all-student",
+    {
+      cache: "no-store",
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error("Fetch data students failed");
+  }
+
+  const studentsInfo = await response.json();
+
+  return studentsInfo;
+}
