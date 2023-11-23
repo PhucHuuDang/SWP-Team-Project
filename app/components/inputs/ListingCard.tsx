b753f4f11ebb5fa35258a0905d8ecf0c452af593
chim347 <nhanntse160792@gmail.com>
2023-11-24 00:00:01 +0700

    11h59 2023/23/11

--- app/components/inputs/ListingCard.tsx
+++ app/components/inputs/ListingCard.tsx
@@ -151,6 +151,10 @@ const ListingCard: React.FC<ListingCardProps> = ({
   //     .join(", ");
   // };
 
+  const formattedPrice = (price: number): string => {
+    return numeral(price).format("0,0 ₫");
+  };
+
   return (
     <div
       // onClick={() => console.log(data.id)}
@@ -252,30 +256,32 @@ const ListingCard: React.FC<ListingCardProps> = ({
           <div className="flex text-md flex-row items-center gap-4">
             <div className="flex flex-row items-center gap-2">
               <del className="font-light text-[#ed9080]">
-                {packageData.totalOriginalPrice}
+                {formattedPrice(packageData.totalOriginalPrice)}
               </del>{" "}
-              <span>$</span>
+              <span>đ</span>
             </div>
 
             <div className="flex flex-row items-center gap-2">
               <span className="font-light text-[#ff6347] ">
-                {packageData.totalPrice}
+                {formattedPrice(packageData.totalPrice)}
               </span>{" "}
-              <span>$</span>
+              <span>đ</span>
             </div>
           </div>
         ) : (
           <div className="flex flex-row text-md items-center gap-4 mt-3">
             <div className="flex flex-row items-center gap-2">
               <del className="font-light text-[#ed9080]">
-                {data?.originalPrice}
+                {formattedPrice(data?.price ?? 0)}
               </del>{" "}
-              <span>$</span>
+              <span>đ</span>
             </div>
 
             <div className="flex flex-row items-center gap-2">
-              <span className="font-light text-[#ff6347] ">{data?.price}</span>{" "}
-              <span>$</span>
+              <span className="font-light text-[#ff6347] ">
+                {formattedPrice(data?.price ?? 0)}
+              </span>{" "}
+              <span>đ</span>
             </div>
           </div>
         )}
